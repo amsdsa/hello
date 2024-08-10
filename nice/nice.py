d2840709be0bb3bbe36e739bc6394593d20173c4
ams <166640511+amsdsa@users.noreply.github.com>
2024-08-10 12:03:10 +0000

    fnice commit

---
+++ nice/nice.py
@@ -0,0 +1,14 @@
+def cool():
+    print("hello")
+    print("great man")
+    return "cool executed"
+    ##ooh ooh
+
+def hello():
+    print("hey man")
+    a=int(5)
+    b=int(8)
+    return "hello executed "+str(a+b)
+#what the asdasd
+cool()
+print(hello())
