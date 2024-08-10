099f68019e6f33410ee7ecebc55663690e84f99a
ams <166640511+amsdsa@users.noreply.github.com>
2024-08-10 14:46:06 +0000

    .

--- new/new.py
+++ new/new.py
@@ -1,2 +1,2 @@
 print("hello")
-print("hello this is cool but the problem is that the people are not that good and this is the major issue which has to be resolved and this if not resolved then this will lead to a lot of problems and this will later on lead to a lot of problems")
+print("helwowowlo this is cool but the problem is that the people are not that good and this is the major issue which has to be resolved and this if not resolved then this will lead to a lot of problems and this will later on lead to a lot of problems")
